6bb3d7a72c9921e8e189922cc61116b2dfee16ec
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-07-07 13:47:04 +0200

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -2,10 +2,24 @@
 #include <array>
 using namespace std;
 
+/*
+since std::pow(base, exp) does not produce compile-time constant
+defining utility::tenToPowersOfPositiveN(n) for this purpose
+in order to use "constexpr" for the arrays for the ranges
+and "template<class T, size_t N> struct array" for the arrays for the frequencies.
+*/
+namespace utility {
+    consteval int tenToPowersOfPositiveN(int n) {
+        int value = 10;
+        while (n-- > 0) { value *= 10; }
+        return value;
+    }
+}
+
 class FrequencyTracker {
 
-    static constexpr array<int, 2> VALUES_RANGE = { 1, 100000 };
-    static constexpr array<int, 2> FREQUENCY_RANGE = { 1, 100000 };
+    static constexpr array<int, 2> VALUES_RANGE = { 1, utility::tenToPowersOfPositiveN(5) };
+    static constexpr array<int, 2> FREQUENCY_RANGE = { 1, utility::tenToPowersOfPositiveN(5) };
 
     array<int, VALUES_RANGE[1] + 1> frequencyOfValues{};
     array<int, FREQUENCY_RANGE[1] + 1> frequencyOfFrequencies{};
@@ -18,7 +32,7 @@ public:
             --frequencyOfFrequencies[frequencyOfValues[value]];
         }
         ++frequencyOfValues[value];
-        ++frequencyOfFrequencies[frequencyOfValues[value]]; 
+        ++frequencyOfFrequencies[frequencyOfValues[value]];
     }
 
     void deleteOne(int value) {
